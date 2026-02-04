59724362b43c4fbc4380e9f5c0937bccf44d6207
kobashix <163171564+kobashix@users.noreply.github.com>
2026-02-04 08:14:08 -0600

    fixing index.html header and footer

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -19,3 +19,32 @@ export async function onRequest(context) {
 
   return context.next();
 }
+export async function onRequest(context) {
+  const { request, next, env } = context;
+  const url = new URL(request.url);
+
+  // Only apply to the homepage
+  if (url.pathname === "/" || url.pathname === "/index.html") {
+    const res = await next();
+    
+    // Fetch Master Components
+    const [headRes, footRes] = await Promise.all([
+      env.ASSETS.fetch(new URL("/header.html", request.url)),
+      env.ASSETS.fetch(new URL("/footer.html", request.url))
+    ]);
+
+    const headerHtml = await headRes.text();
+    const footerHtml = await footRes.text();
+
+    return new HTMLRewriter()
+      .on(".site-header", { 
+        element(el) { el.setInnerContent(headerHtml, { html: true }); } 
+      })
+      .on(".site-footer", { 
+        element(el) { el.setInnerContent(footerHtml, { html: true }); } 
+      })
+      .transform(res);
+  }
+
+  return next();
+}
